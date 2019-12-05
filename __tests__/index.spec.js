7733c3faf4d619d403ca66f972fda69911a4438e
Raphael Thomazella <thomazella9@gmail.com>
2019-12-05 09:47:41 -0300

    chore(misc): more tests, build setup

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,4 +1,4 @@
-import { addToGlobalConsole, logs } from '../src'
+import { addToGlobalConsole, logs, logk } from '../src'
 import pf from 'pretty-format'
 
 beforeAll(() => {
@@ -14,16 +14,41 @@ beforeEach(() => {
     }
 })
 
-describe('lib test', () => {
-    it('addToGlobalConsole adds functions to console', () => {
-        addToGlobalConsole()
-        expect(console.logk).toBeDefined()
-        expect(console.logs).toBeDefined()
-    })
-    it('logs uses pretty-format', () => {
+describe('logs', () => {
+    it('uses pretty-format', () => {
         jest.spyOn(global.console, 'log').mockImplementation(() => {})
         const input = { foo: 2 }
         logs(input)
         expect(console.log).toHaveBeenCalledWith('console.logs ', pf(input))
+    })
+    it('uses passes strings thru', () => {
+        jest.spyOn(global.console, 'log').mockImplementation(() => {})
+        const input = 'input'
+        logs(input)
+        expect(console.log).toHaveBeenCalledWith('console.logs ', input)
+    })
+    it('strings and object case', () => {
+        jest.spyOn(global.console, 'log').mockImplementation(() => {})
+        logs('input', { foo: 2 })
+        expect(console.log).toHaveBeenNthCalledWith(1, 'console.logs ', 'input')
+        expect(console.log).toHaveBeenNthCalledWith(2, 'console.logs ', pf({ foo: 2 }))
+    })
+    it('strings and object case, many params', () => {
+        jest.spyOn(global.console, 'log').mockImplementation(() => {})
+        logs('input', { foo: 2 }, 'bar', { baz: { bin: 33, gor: false } }, 'melt', 'pelt', {})
+        expect(console.log).toHaveBeenNthCalledWith(1, 'console.logs ', 'input')
+        expect(console.log).toHaveBeenNthCalledWith(2, 'console.logs ', pf({ foo: 2 }))
+        expect(console.log).toHaveBeenNthCalledWith(3, 'console.logs ', 'bar')
+        expect(console.log).toHaveBeenNthCalledWith(4, 'console.logs ', pf({ baz: { bin: 33, gor: false } }))
+        expect(console.log).toHaveBeenNthCalledWith(5, 'console.logs ', 'melt')
+        expect(console.log).toHaveBeenNthCalledWith(6, 'console.logs ', 'pelt')
+        expect(console.log).toHaveBeenNthCalledWith(7, 'console.logs ', pf({}))
+    })
+})
+describe('addToGlobalConsole', () => {
+    it('addToGlobalConsole adds functions to console', () => {
+        addToGlobalConsole()
+        expect(console.logk).toBe(logk)
+        expect(console.logs).toBe(logs)
     })
 })
--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const logs: (...args: any[]) => void;
-export declare const logk: (...args: any[]) => void;
-declare const _default: {
-    logs: (...args: any[]) => void;
-    logk: (...args: any[]) => void;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
